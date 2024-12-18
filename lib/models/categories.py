4b9005bccb5778ba64e488a3d270039147d74057
Perpetual Akinyi <you@example.com>
2024-12-18 11:16:01 +0300

    creting a table

--- lib/models/categories.py
+++ lib/models/categories.py
@@ -1,8 +1,17 @@
 class Categories:
-    all = {}
+    all = {}#this is a dict that will hold all the categories by there (id)
 
-    def __init__(self, id, name):
+    def __init__(self, name, id= None): #this is the initialization
         self.id = id
         self.name = name
+
+    @classmethod
+    def add_category(cls, name):
+        pass
+
+    def __repr__(self):
+        return f"<Categories {self.id}: {self.name}>" #making sure that when we print a category it will look like this as strings
+
+    
 
       
